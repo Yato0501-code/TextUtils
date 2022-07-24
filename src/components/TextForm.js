3f070407a139dceda238ed59224a8beb1955ad6a
Yash <tomaryash621@gmail.com>
2022-07-24 11:51:39 +0530

    Update TextForm.js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -36,14 +36,14 @@ export default function TextForm(props) {
         <div className="mb-3">
             <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'#042743'}} id="my-box" rows="8"></textarea>
         </div>
-        <button className="btn btn-primary mx-2" onClick={handleUpCLick}>Convert to Uppercase</button>
-        <button className="btn btn-primary mx-2" onClick={handleLoCLick}>Convert to Lowercase</button>
-        <button className="btn btn-primary mx-2" onClick={handleClearCLick}>Clear Text</button>
-        <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>Clear Extra Spaces</button>
+        <button className="btn btn-primary mx-2 my-1" onClick={handleUpCLick}>Convert to Uppercase</button>
+        <button className="btn btn-primary mx-2 my-1" onClick={handleLoCLick}>Convert to Lowercase</button>
+        <button className="btn btn-primary mx-2 my-1" onClick={handleClearCLick}>Clear Text</button>
+        <button className="btn btn-primary mx-2 my-1" onClick={handleExtraSpaces}>Clear Extra Spaces</button>
     </div>
     <div className="container my-2" style={{color: props.mode==='dark'?'white':'#042743'}}>
       <h2>Your text summary</h2>
-      <p>{text.split(" ").filter((element)=>{ return element.length!= 0 }).length} words and {text.length} characters</p>
+      <p>{text.split(" ").filter((element)=>{ return element.length!== 0 }).length} words and {text.length} characters</p>
       <p>{0.008 * text.split(" ").length} Minutes to read</p>
       <h2>Preview</h2>
       <p>{text.length>0?text:'Enter something to above preview it here'}</p>
